6b6eb1549301855895be2de1d7768e473845961d
Nguyen Van Anh Tuan <anhtuanqn1002@gmail.com>
2016-03-28 08:38:18 +0700

    Add .gitignore and create project

---
+++ HelloCocos2D/Classes/HelloWorldScene.h
@@ -0,0 +1,61 @@
+//
+//  HelloWorldScene.h
+//
+//  Created by : Nguyen Van Anh Tuan
+//  Project    : HelloCocos2D
+//  Date       : 3/28/16
+//
+//  Copyright (c) 2016 Nguyen Van Anh Tuan.
+//  All rights reserved.
+//
+// -----------------------------------------------------------------
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+#import "cocos2d-ui.h"
+
+// -----------------------------------------------------------------------
+
+@interface HelloWorldScene : CCScene
+
+// -----------------------------------------------------------------------
+
+- (instancetype)init;
+
+// -----------------------------------------------------------------------
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
