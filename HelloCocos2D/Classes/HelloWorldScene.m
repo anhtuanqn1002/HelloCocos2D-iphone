21aad74a79f815eff35e4b1fddc6f60eb28c20d0
Nguyen Van Anh Tuan <anhtuanqn1002@gmail.com>
2016-03-31 16:43:54 +0700

    demo sounds

--- HelloCocos2D/Classes/HelloWorldScene.m
+++ HelloCocos2D/Classes/HelloWorldScene.m
@@ -15,12 +15,17 @@
 #import "OALAudioTrack.h"
 #import "OALAudioTracks.h"
 #import "OALAudioSession.h"
+#import "CCFileUtils.h"
 
 // -----------------------------------------------------------------
 
 @interface HelloWorldScene ()
 
 @property (nonatomic, strong) OALSimpleAudio *audio;
+@property (nonatomic, strong) OALAudioTrack *track;
+@property (nonatomic, strong) OALAudioTracks *listAudio;
+@property (nonatomic, strong) OALAudioTrack *t;
+@property (nonatomic, strong) NSMutableArray *l;
 
 @end
 
@@ -40,30 +45,25 @@
     // Just make an assert, so that you can catch it in debug
     NSAssert(self, @"Whoops");
     
-    // Background
-    CCSprite9Slice *background = [CCSprite9Slice spriteWithImageNamed:@"white_square.png"];
-    background.anchorPoint = CGPointZero;
-    background.contentSize = [CCDirector sharedDirector].viewSize;
-    background.color = [CCColor grayColor];
-    [self addChild:background];
-    
-    // The standard Hello World text
-    CCLabelTTF *label = [CCLabelTTF labelWithString:@"Hello World" fontName:@"ArialMT" fontSize:64];
-    label.positionType = CCPositionTypeNormalized;
-    label.position = (CGPoint){0.5, 0.5};
-    [self addChild:label];
+//    // Background
+//    CCSprite9Slice *background = [CCSprite9Slice spriteWithImageNamed:@"white_square.png"];
+//    background.anchorPoint = CGPointZero;
+//    background.contentSize = [CCDirector sharedDirector].viewSize;
+//    background.color = [CCColor grayColor];
+//    [self addChild:background];
+//    
+//    // The standard Hello World text
+//    CCLabelTTF *label = [CCLabelTTF labelWithString:@"Hello World" fontName:@"ArialMT" fontSize:64];
+//    label.positionType = CCPositionTypeNormalized;
+//    label.position = (CGPoint){0.5, 0.5};
+//    [self addChild:label];
     
     //---------------------------------------------------
-    
+    // OALSimpleAudio
     // --- Background music
     self.audio = [OALSimpleAudio sharedInstance];
-    [self.audio playBg:@"Dragonball.mp3" volume:1.0 pan:0 loop:YES];
-//    self.audio
-//    OALAudioTrack *track = [OALAudioTrack track];
-//    [track playUrl:[NSURL URLWithString:@"http://mp3.zing.vn/album/De-Trai-Tim-Nghi-Ngoi-Khac-Viet/ZWZCOIBC.html?st=2"]];
-//    OALAudioTrack *track = [OALAudioTrack new];
-//    [track playFile:@"ChocoboRacing.mp3"];
-//    [[track player] play];
+//    [self.audio playBg:@"LKDJ.mp3" volume:1.0 pan:0 loop:YES];
+    [self.audio playBg:@"Dragonball.mp3"];
     
     // --- Effects music
     // 1
@@ -95,11 +95,45 @@
     [stop setTarget:self selector:@selector(touchInsideStop:)];
     
     //---------------------------------------------------
+    // OALAudioTrack & OALAudioTracks
+    self.listAudio = [OALAudioTracks new];
+    
+    // 5
+    OALAudioTrack *track1 = [OALAudioTrack track];
+    [track1 preloadFile:@"Powerup9.wav"];
+    [track1 play];
+    
+    // 6
+    OALAudioTrack *track2 = [OALAudioTrack track];
+    [track2 preloadFile:@"Dragonball.mp3"];
+    
+    // 7
+    OALAudioTrack *track3 = [OALAudioTrack track];
+    [track3 preloadFile:@"LKDJ.mp3"];
+    
+    // 8
+    self.l = [NSMutableArray new];
+    [self.l addObjectsFromArray:@[track1, track2, track3]];
+    [[self.listAudio tracks] arrayByAddingObjectsFromArray:self.l];
+    [(OALAudioTrack *)[[self.listAudio tracks] objectAtIndex:0] play];
+    for (NSInteger i = 1; i < [self.listAudio tracks].count; i++) {
+        [[[self.listAudio tracks] objectAtIndex:i] playAfterTrack:[[self.listAudio tracks] objectAtIndex:i-1]];
+    }
+    
+    //---------------------------------------------------
+    // OALAudioSession
+    
     
     // done
     return self;
 }
 
+- (void)update:(CCTime)delta {
+//    CCLOG(@"bbbb - %d", [self.track play]);
+}
+- (void)playTrack:(id)sender {
+    CCLOG(@"aaaa");
+}
 - (void)touchInside1:(id)sender {
     NSLog(@"Touch up inside a button1");
     [self.audio playEffect:@"Powerup7.wav"];
@@ -122,6 +156,7 @@
 - (void)touchInsideStop:(id)sender {
     NSLog(@"Touch up inside stop");
     [self.audio stopEverything];
+    [self.listAudio stopAllTracks];
 }
 
 
